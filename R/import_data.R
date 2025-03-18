eb6b9c10e386e189636a50ec3228ab94c0413451
aestes3 <aidanestes@gmail.com>
2025-03-18 10:26:36 -0400

    Update import_data.R

--- R/import_data.R
+++ R/import_data.R
@@ -1,1 +1,11 @@
 #### TEST File for Importing Data
+# Load Packages
+library(tidyverse)
+library(here)
+library(readxl)
+
+#Import Translated Data 
+data <- read_xlsx(here("data-raw/Data Translated.xlsx"), sheet = "data")
+#Export excel data to Rdata
+saveRDS(data, file = here("data/data.RData"))
+
