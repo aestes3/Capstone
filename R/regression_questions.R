a68763e993a03a173546c89ca198b76523d6faae
aestes3 <aidanestes@gmail.com>
2025-03-25 14:24:00 -0400

    Fixed Tables and Regression equations
    Equations are now using Poisson Regression instead of Log-Binomial. This leads to an estimation of the prevalence ratio rather than an exact value.

--- R/regression_questions.R
+++ R/regression_questions.R
@@ -19,17 +19,46 @@ clean <- clean_data
 clean_use <- clean |> 
     filter(population != 3)
 
+
+#### GROUP VARIABLES ####
+# Combine factor variables to create larger groups
+# Remove NAs in specific variables being evaluated
+clean_grouped <- clean_use |> 
+#Code multi-variate variables
+mutate(gender = case_when(
+    gender == 0 ~ 0,
+    gender == 1 ~ 1,
+    gender == 2 ~ 2,
+    gender == 3 ~ 2, #Code from unknown to combined category of Transfemale/unknown to other
+    TRUE ~ as.numeric(NA)
+)) |>
+mutate(nationality = case_when(
+    nationality == 0 ~ 0, #Native
+    nationality == 1 ~ 1, #Forigen 
+    nationality == 2 ~ 1,
+    nationality == 3 ~ 1,
+    TRUE ~ as.numeric(NA)
+)) |> 
+mutate(sexual_orientation = case_when(
+    sexual_orientation == 0 ~ 0, #Sraight Vs Other
+    sexual_orientation == 1 ~ 1,
+    sexual_orientation == 2 ~ 1,
+    sexual_orientation == 3 ~ 1,
+    sexual_orientation == 4 ~ 1,
+)) 
+    
+
 ### Question 1: Influence of Demographic factors on HIV intervention components (condoms, lubricants)
-model1_condoms <- glm(condoms ~ age + sex + gender + nationality + sexual_orientation + ethnicity,
-               family = binomial (link='logit'), data=clean_use)
+model1_condoms <- glm(condoms ~ age + as.factor(gender) + nationality + sexual_orientation,
+               family = poisson (link='log'), data=clean_grouped)
 
 #Create condoms table
-table_condoms <- model1_condoms |> tbl_regression()
+table_condoms <- model1_condoms |> tbl_regression(exponentiate = TRUE)
 
-model1_lubricants <-glm(lubricants ~ age + sex + gender + nationality + sexual_orientation + ethnicity,
-                                 family = binomial (link='logit'), data=clean_use)
+model1_lubricants <-glm(lubricants ~ age + as.factor(gender) + nationality + sexual_orientation,
+                                 family = poisson (link='log'), data=clean_grouped)
 #Create lubricants table
-table_lubricants <- model1_lubricants |> tbl_regression()
+table_lubricants <- model1_lubricants |> tbl_regression(exponentiate = TRUE)
 
 #Merge tables
 tbl_merge(
@@ -39,10 +68,10 @@ tbl_merge(
 
 ### Question 2: Factors associated with access to HIV care among individuals who tested pos
 
-df_positive <- filter(clean_use, first_test == "1")
-model2_careaccess <- glm(care_access ~ age + sex + gender + nationality + sexual_orientation + ethnicity,
-                         family = binomial (link='logit'), data=df_positive)
+df_positive <- filter(clean_grouped, first_test == "1")
+model2_careaccess <- glm(care_access ~ age + as.factor(gender) + nationality + sexual_orientation,
+                         family = poisson (link='log'), data=df_positive)
 
-model2_careaccess |> tbl_regression(intercept = TRUE) |> 
+model2_careaccess |> tbl_regression(intercept = TRUE, exponentiate = TRUE) |> 
     add_glance_source_note() |> 
     add_global_p()
